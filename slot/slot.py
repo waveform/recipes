053f3ffe855964b49a987390a51eeff242d30b28
Stanley Wang <Stanley.Wang@arm.com>
2018-04-16 15:30:41 +0100

    Use py3.6 f-string, and fixed 'else:' incorrect indent issue.

--- slot/slot.py
+++ slot/slot.py
@@ -3,14 +3,14 @@
 import re
 from io import StringIO
 class template(object):
-    indent_dec_keyword = re.compile(r'^(elif|else|except|finally)\s+')
+    indent_dec_keyword = re.compile(r'^(elif|else|except|finally)[^0-9a-zA-Z_]+')
     def __init__(self, tmpl):
         self.text = self.code = ''
         deep, indent = 0, ' ' * 4
         w = StringIO()
         for l in StringIO(tmpl):
             if l[0] != '$':
-                w.write("{0}_output.append({1}.format(**locals()))\n".format(indent * deep, repr(l[:-1])))
+                w.write("{0}_output.append(f{1})\n".format(indent * deep, repr(l[:-1])))
                 continue
             l = l[1:].strip()
             if l == 'end':
